1522638084a81cbb92933f6a1f11798661390ff7
노준석 <joshua052698@gmail.com>
2025-04-07 16:17:18 +0900

    feat: Update production configuration to use PostgreSQL and load environment variables

--- config/production.py
+++ config/production.py
@@ -1,7 +1,14 @@
 from logging.config import dictConfig
 from config.default import *
+from dotenv import load_dotenv
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'pybo.db'))
+load_dotenv(os.path.join(BASE_DIR, '.env'))
+
+SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(
+    user=os.getenv('DB_USER'),
+    pw=os.getenv('DB_PASSWORD'),
+    url=os.getenv('DB_HOST'),
+    db=os.getenv('DB_NAME'))
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = b'\xbf\xa7\xf7\xc3[[\xbeP8v19\xc2\xaa\xe8\xe1'
 
